b9e20ffb4cdc4b594f4f86af9404270db89c0d78
KlnMifflin <klnmifflin@gmail.com>
2025-08-16 11:06:00 -0400

    Improve Appearance, Add Icons
    Improve the appearance of banner with auto-adjusting text, add light and dark icons

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const toggleThemeBtn = document.getElementById("themeToggle");
 toggleThemeBtn.addEventListener("click", () => {
     document.body.classList.toggle("dark");
-    toggleThemeBtn.textContent = document.body.classList.contains("dark") ? "☀️ Light Mode" : "🌒 Dark Mode";
+    let isDark = document.body.classList.contains("dark");
+    document.getElementById("icon-mj").src = isDark ? "res/mahjong-icon-dark.png" : "res/mahjong-icon.png";
+    document.getElementById("icon-ai").src = isDark ? "res/ai-icon-dark.png" : "res/ai-icon.png";
+    toggleThemeBtn.textContent = isDark ? "☀️ Light Mode" : "🌒 Dark Mode";
 })
