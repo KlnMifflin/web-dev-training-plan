f5c1b497bae1d489e6e583298a1218ea75e7d4b0
KlnMifflin <klnmifflin@gmail.com>
2025-08-16 19:01:35 -0400

    Add Skill & Interest Icons
    Add skill and interest icons, add link to chess page, improve styling.

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ const toggleThemeBtn = document.getElementById("themeToggle");
 toggleThemeBtn.addEventListener("click", () => {
     document.body.classList.toggle("dark");
     let isDark = document.body.classList.contains("dark");
+    toggleThemeBtn.textContent = isDark ? "☀️ Light Mode" : "🌒 Dark Mode";
     document.getElementById("icon-mj").src = isDark ? "res/mahjong-icon-dark.png" : "res/mahjong-icon.png";
     document.getElementById("icon-ai").src = isDark ? "res/ai-icon-dark.png" : "res/ai-icon.png";
-    toggleThemeBtn.textContent = isDark ? "☀️ Light Mode" : "🌒 Dark Mode";
+    document.getElementById("icon-web").src = isDark ? "res/web-dev-icon-dark.png" : "res/web-dev-icon.png";
+    document.getElementById("icon-cpp").src = isDark ? "res/cpp-icon-dark.png" : "res/cpp-icon.png";
+    document.getElementById("icon-m365").src = isDark ? "res/m365-icon-dark.png" : "res/m365-icon.png";
+    document.getElementById("icon-robot").src = isDark ? "res/robot-icon-dark.png" : "res/robot-icon.png";
+    document.getElementById("icon-chess").src = isDark ? "res/chess-icon-dark.png" : "res/chess-icon.png";
+    document.getElementById("icon-rugby").src = isDark ? "res/rugby-icon-dark.png" : "res/rugby-icon.png";
+
 })
