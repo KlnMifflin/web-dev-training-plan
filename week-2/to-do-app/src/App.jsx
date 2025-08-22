84539a54744da711b7eb8a105152cbbf54acdea8
KlnMifflin <klnmifflin@gmail.com>
2025-08-22 13:26:22 -0400

    Complete List and Item Addition/Deletion
    Complete integration of list, including addition and deletion of lists and items while updating state.

--- week-2/to-do-app/src/App.jsx
+++ week-2/to-do-app/src/App.jsx
@@ -2,17 +2,18 @@ import { Routes, Route, Link } from "react-router-dom";
 import { useState } from "react";
 import Home from "./components/Home";
 import ListPage from "./components/ListPage";
+import Header from "./components/Header";
 
 export default function App() {
 
     const [lists, setLists] = useState([
-        {id: 1+"-listy", name: "listy", items: ["wha"]},
-        {id: 2+"pepper", name: "pepper", items: ["huh", "nuh"]},
-        {id: 3+"water", name: "water", items: []}
+        {id: 1+"-listy", name: "listy", items: []},
+        {id: 2+"-pepper", name: "pepper", items: [{name: "huh", done: false}, {name: "nuh", done: true}]},
     ])
 
     return (
         <div>
+            <Header />
             <Routes>
                 {/* Show all lists */}
                 <Route path="/" element={<Home lists={lists} setLists={setLists} />} />
